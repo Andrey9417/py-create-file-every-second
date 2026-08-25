4f88ec2528eaf83448f2526b04c838f37c5937f5
Andrey <andreypovlat@gmail.com>
2026-08-25 17:37:02 +0300

    Task: Create file every second

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        file_name = datetime.now().strftime("app-%H_%M_%S.log")
+        file_content = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open(file_name, "w") as file:
+            file.write(file_content)
+        print(file_content, file_name)
+        sleep(1)
 
 
 if __name__ == "__main__":
